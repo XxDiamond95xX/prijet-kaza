70a2d20576d2912f58db5fadcfd3fe12c8956057
XxDiamond95 <c.mamady@outlook.fr>
2024-03-06 11:40:28 +0100

    mise en place du responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import LogoKasa from '@assets/LOGO.svg'
 import { Link } from 'react-router-dom';
 
 function Header() {
-    return(
+    return (
         <>
             <header>
-                <img  src={LogoKasa} alt="Logo de Kasa" />
+                <img className='Logo' src={LogoKasa} alt="Logo de Kasa" />
                 <nav>
                     <Link to='/'>Accueil</Link>
                     <Link to='/about'>A propos</Link>
