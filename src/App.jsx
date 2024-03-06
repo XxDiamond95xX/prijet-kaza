d49971dd185a54d79bb6305bf8f9db81f5391944
XxDiamond95 <c.mamady@outlook.fr>
2024-03-06 02:31:29 +0100

    page about terminer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import './App.css'
 import Homepage from './pages/Homepage'
 import About from './pages/About'
 import Location from './pages/Location'
+// Importez les icônes que vous souhaitez utiliser
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+
+// Configurer Font Awesome pour utiliser les icônes que vous avez importées
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faChevronUp);
+
 
 function App() {
 
--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,38 +1,79 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import '../../App.css';
+
 function Info() {
+    const [isFiabilityVisible, setIsFiabilityVisible] = useState(false);
+    const [isRespectVisible, setIsRespectVisible] = useState(false);
+    const [isServiceVisible, setIsServiceVisible] = useState(false);
+    const [isSecurityVisible, setIsSecurityVisible] = useState(false);
+
+    const toggleFiabilityVisibility = () => {
+        setIsFiabilityVisible(!isFiabilityVisible);
+    };
+
+    const toggleRespectVisibility = () => {
+        setIsRespectVisible(!isRespectVisible);
+    };
+
+    const toggleServiceVisibility = () => {
+        setIsServiceVisible(!isServiceVisible);
+    };
+
+    const toggleSecurityVisibility = () => {
+        setIsSecurityVisible(!isSecurityVisible);
+    };
     return (
         <>
             <div className="Info">
-                <div className="Info_title">
+                <div className="Info_title" onClick={toggleFiabilityVisibility}>
                     Fiabilité
+                    <FontAwesomeIcon
+                        icon={faChevronUp}
+                        className={isFiabilityVisible ? 'rotate180' : 'rotate0'}
+                    />
                 </div>
-                <div className="Info_title_p">
-                    Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et<br />
-                    toutes les informations sont régulièrement vérifiées par nos équipes.
+                <div className={`Info_title_p ${isFiabilityVisible ? 'visible' : 'hidden'}`}>
+                    Les annonces postées sur kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et<br />
+                    toutes les informations sont réguliereement vérifiées par nos équipes.
                 </div>
             </div>
             <div className="Info">
-                <div className="Info_title">
+                <div className="Info_title" onClick={toggleRespectVisibility}>
                     Respect
+                    <FontAwesomeIcon
+                        icon={faChevronUp}
+                        className={isRespectVisible ? 'rotate180' : 'rotate0'}
+                    />
                 </div>
-                <div className="Info_title_p">
+                <div className={`Info_title_p ${isRespectVisible ? 'visible' : 'hidden'}`}>
                     La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de<br />
                     perturbation du voisinage entraînera une exclusion de notre plateforme.
                 </div>
             </div>
             <div className="Info">
-                <div className="Info_title">
+                <div className="Info_title" onClick={toggleServiceVisibility}>
                     Service
+                    <FontAwesomeIcon
+                        icon={faChevronUp}
+                        className={isServiceVisible ? 'rotate180' : 'rotate0'}
+                    />
                 </div>
-                <div className="Info_title_p">
+                <div className={`Info_title_p ${isServiceVisible ? 'visible' : 'hidden'}`}>
                     La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de<br />
                     perturbation du voisinage entraînera une exclusion de notre plateforme.
                 </div>
             </div>
             <div className="Info">
-                <div className="Info_title">
+                <div className="Info_title" onClick={toggleSecurityVisibility}>
                     Sécurité
+                    <FontAwesomeIcon
+                        icon={faChevronUp}
+                        className={isSecurityVisible ? 'rotate180' : 'rotate0'}
+                    />
                 </div>
-                <div className="Info_title_p">
+                <div className={`Info_title_p ${isSecurityVisible ? 'visible' : 'hidden'}`}>
                     La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement<br />
                     correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au<br />
                     locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons<br />
