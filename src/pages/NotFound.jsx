9e4259f3c3c881da0de570f6328f8123dc45cafe
XxDiamond95 <c.mamady@outlook.fr>
2024-03-18 16:24:22 +0100

    correction css

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <>
             <Header />
             <div className='error'>
-                <h2>Page Not Found</h2>
-                <p>Sorry, the page you are looking for does not exist.</p>
+                <h2>404</h2>
+                <p>Oups! La page que vous demandez n'existe pas</p>
+                <Link to='/' className='error_Link'>Retournez sur la page d'accueil</Link>
             </div>
             <Footer />
         </>
